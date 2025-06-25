72c47dfc5bec81f42cf3aa5519e92ef033451519
Igor Gurchenko <igorivanov122@gmail.com>
2025-06-25 18:55:40 +0300

    Added connection to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +80,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME').strip(),
+        'USER': os.getenv('USER').strip(),
+        'PASSWORD': os.getenv('PASSWORD').strip(),
+        'HOST': os.getenv('HOST').strip(),
+        'PORT': os.getenv('PORT').strip(),
     }
 }
 

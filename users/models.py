991dc13b7a08fc2d3db410be4d7c0bbf63668041
Igor Gurchenko <igorivanov122@gmail.com>
2025-06-28 15:41:13 +0300

    Added lessons to Course serializer and Payment filters

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from materials.models import Course, Lesson
 
 
 class CustomUserManager(BaseUserManager):
@@ -52,3 +54,24 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    CASH = 'cash'
+    TRANSFER = 'transfer'
+
+    PAYMENT_METHOD_CHOICES = [
+        (CASH, 'Наличные'),
+        (TRANSFER, 'Перевод на счёт'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='payments', verbose_name='Пользователь')
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True, related_name='course_payments', verbose_name='Оплаченный курс')
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson_payments', verbose_name='Оплаченный урок')
+    payment_date = models.DateField(auto_now_add=True, verbose_name='Дата оплаты')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        item = self.paid_course.title if self.paid_course else self.paid_lesson.title
+        return f'{self.user.email} - {item} - {self.amount}₽'
---
+++ users/views.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Payment
+from .serializers import PaymentSerializer
+from .filters import PaymentFilter
+
+
+class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = PaymentFilter
+    ordering_fields = ['payment_date']
+    ordering = ['-payment_date']

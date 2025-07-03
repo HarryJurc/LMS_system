ae500b475e3f6bed514c560e075164ad3fc578bd
Igor Gurchenko <igorivanov122@gmail.com>
2025-07-03 21:28:02 +0300

    Added user CRUD, JWT auth, permissions, moderator group

--- materials/models.py
+++ materials/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+from django.conf import settings
+
 
 class Course(models.Model):
     """Модель курса"""
     title = models.CharField(max_length=255, verbose_name='Название')
     preview = models.ImageField(upload_to='course_previews/', blank=True, null=True, verbose_name='Превью')
     description = models.TextField(blank=True, verbose_name='Описание')
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='courses',
+        verbose_name='Владелец',
+        null=True
+    )
 
     def __str__(self):
         return self.title
@@ -18,6 +27,13 @@ class Lesson(models.Model):
     description = models.TextField(blank=True, verbose_name='Описание')
     preview = models.ImageField(upload_to='lesson_previews/', blank=True, null=True, verbose_name='Превью')
     video_url = models.URLField(verbose_name='Ссылка на видео')
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='lessons',
+        verbose_name='Владелец',
+        null=True
+    )
 
     def __str__(self):
         return f'{self.title} (Курс: {self.course.title})'
--- users/views.py
+++ users/views.py
@@ -1,9 +1,18 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions, generics
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Payment
-from .serializers import PaymentSerializer
-from .filters import PaymentFilter
+from rest_framework.exceptions import PermissionDenied
 
+from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
+from .models import Payment
+from .permissions import IsModeratorOrReadOnly
+from .serializers import UserSerializer, PaymentSerializer, RegisterSerializer, UserPublicSerializer
+from .filters import PaymentFilter
+from django.contrib.auth import get_user_model
+
+
+
+User = get_user_model()
 
 class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Payment.objects.all()
@@ -12,3 +21,42 @@ class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
     filterset_class = PaymentFilter
     ordering_fields = ['payment_date']
     ordering = ['-payment_date']
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.user.id == self.kwargs.get('pk', None) or self.action == 'create':
+            return UserSerializer
+        if self.action in ['retrieve', 'list']:
+            return UserPublicSerializer
+        return UserSerializer
+
+    def get_queryset(self):
+        return User.objects.all()
+
+    def perform_update(self, serializer):
+        if self.get_object() != self.request.user:
+            raise PermissionDenied("You can only edit your own profile.")
+        serializer.save()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsModeratorOrReadOnly]
+
+
+class LessonViewSet(viewsets.ModelViewSet):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+    permission_classes = [IsModeratorOrReadOnly]
+

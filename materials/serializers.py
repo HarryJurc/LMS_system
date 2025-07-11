af5628596984deef601b350d5a512d135632c441
Igor Gurchenko <igorivanov122@gmail.com>
2025-07-11 19:27:56 +0300

    Integrate Stripe for course payments

--- materials/serializers.py
+++ materials/serializers.py
@@ -17,7 +17,7 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ['id', 'title', 'preview', 'description', 'lessons_count', 'lessons']
+        fields = ['id', 'title', 'preview', 'description', 'lessons_count', 'lessons', 'is_subscribed']
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
